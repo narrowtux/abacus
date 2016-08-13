607399451b62960d16e6facede55dcaaafad8a28
Moritz Schmale <narrow.m@gmail.com>
2016-08-13 13:39:41 +0200

    Added tests for formatting

---
+++ test/format_test.exs
@@ -0,0 +1,27 @@
+defmodule FormatTest do
+  use ExUnit.Case
+  doctest Abacus.Format
+  import Abacus, only: [format: 1]
+
+  describe "should format" do
+    test "basic operators" do
+      assert format("a + b") == {:ok, "a + b"}
+      assert format("1 - b") == {:ok, "1 - b"}
+      assert format("1 / b") == {:ok, "1 / b"}
+      assert format("1 * b") == {:ok, "1 * b"}
+      assert format("1^b") == {:ok, "1 ^ b"}
+    end
+
+    test "grouping" do
+      assert format("2 - (2 - 3)") == {:ok, "2 - (2 - 3)"}
+      assert format("2 + (2 + 2)") == {:ok, "2 + (2 + 2)"}
+      assert format("(2 + 2) + 2") == {:ok, "2 + 2 + 2"}
+      assert format("(1 + 2) * (3 - 4)") == {:ok, "(1 + 2) * (3 - 4)"}
+    end
+
+    test "functions" do
+      assert format("sin(2)") == {:ok, "sin(2)"}
+      assert format("function_with_arguments(1, 2, (2 + 3))") == {:ok, "function_with_arguments(1, 2, 2 + 3)"}
+    end
+  end
+end
