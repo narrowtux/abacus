7fb80314941440521f135f122fd6d9b64ac1cfcb
Moritz Schmale <narrow.m@gmail.com>
2016-08-13 19:05:44 +0200

    Faster factorial

--- lib/util.ex
+++ lib/util.ex
@@ -1,4 +1,12 @@
 defmodule Abacus.Util do
   def factorial(0), do: 1
-  def factorial(n) when n > 0, do: n * factorial(n - 1)
+  def factorial(n) when n > 0 do
+    _factorial(1, n, 1)
+  end
+
+  def _factorial(n, m, r) when n < m do
+     _factorial(n + 1, m, r * (n + 1))
+  end
+
+  def _factorial(n, n, r), do: r
 end
