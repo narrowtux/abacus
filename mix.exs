f19f3fc7a20f9e4a53a5b1b46eb2ce6ec36fc1cb
narrowtux <narrow.m@gmail.com>
2026-03-30 11:52:52 +0200

    release 2.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Abacus.Mixfile do
   def project do
     [
       app: :abacus,
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
